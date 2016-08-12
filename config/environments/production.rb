cb44cfaeda354b4929ccf9c01396d54f7a5d0cf5
Graham Plata <graham.plata@gmail.com>
2016-08-12 19:07:48 -0400

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = {host: 'https://app54704260@heroku.com'}
+  config.action_mailer.default_url_options = {host: 'https://www.graham-demo.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
